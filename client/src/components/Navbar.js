051c27c9912bd2d565b9c1e0bb21740b73792a6d
ericnam <ericnam86@utexas.edu>
2018-02-20 02:41:39 -0600

    Navbar drawer update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,8 @@ import FlatButton from 'material-ui/FlatButton';
 import Drawer from 'material-ui/Drawer';
 import FontIcon from 'material-ui/FontIcon';
 import Reorder from 'material-ui/svg-icons/action/reorder';
+import Clear from 'material-ui/svg-icons/content/clear';
+
 
 export default class Navbar extends Component {
 	constructor(props) {
@@ -40,7 +42,17 @@ export default class Navbar extends Component {
 					    iconElementRight={rightNav}
 			  		/>
       			</div>
-	      		<Drawer width={200} openSecondary={true} open={this.state.open} >
+	      		<Drawer width={250} openSecondary={true} open={this.state.open}>
+	      			<div className="nav-item-list drawer">
+		      			<FlatButton className="navbar-item drawer" label="ABOUT" />
+				    	<FlatButton className="navbar-item drawer" label="WORK" />
+				    	<FlatButton className="navbar-item drawer" label="CONTACT" />
+	      			</div>
+			    	<FlatButton
+			    		className="navbar-item-icon drawer"
+				        icon={<Clear />}
+				        onClick={this.handleToggle}
+			    	/>
 		        </Drawer>
       		</div>
     	)
